e0f53130ee918c89d14193aa79aba66cf7cadb0f
Nika Golestani <ng8224@gmail.com>
2023-09-05 23:37:59 +0300

    make the operations -+*/ from+

--- main.go
+++ main.go
@@ -9,29 +9,53 @@ import (
 	"strings"
 )
 
-func main(){
-	reader:=bufio.NewReader(os.Stdin)
+func main() {
+	reader := bufio.NewReader(os.Stdin)
 
 	//Get value 1
 	fmt.Println("Enter value 1:")
-	input1 , _:=reader.ReadString('\n')
-	float1 , err :=strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err!=nil{
+	input1, _ := reader.ReadString('\n')
+	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+	if err != nil {
 		fmt.Println(err)
 		panic("Nooooooooo You have to enter a number ")
 	}
 
 	//Get value 2
 	fmt.Println("Enter value 2:")
-	input2 , _:=reader.ReadString('\n')
-	float2 , err :=strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err!=nil{
+	input2, _ := reader.ReadString('\n')
+	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	if err != nil {
 		fmt.Println(err)
 		panic("Nooooooooo You have to enter a number ")
 	}
+	i := 0
 
-	//Get the sum
-	mySum:=math.Round((float1+float2)*100)/100
-	fmt.Println(mySum)
+	for i != 1 {
+		fmt.Println("What should I do:(+ - *  /)")
+		input3, _ := reader.ReadString('\n')
+		equation1 := strings.TrimSpace(input3)
+		//Get the sum
+		switch equation1 {
+		case "+":
+			result := math.Round((float1+float2)*100) / 100
+			fmt.Println(result)
+			i = 1
+		case "-":
+			result := math.Round((float1-float2)*100) / 100
+			fmt.Println(result)
+			i = 1
+		case "*":
+			result := math.Round((float1*float2)*100) / 100
+			fmt.Println(result)
+			i = 1
+		case "/":
+			result := math.Round((float1/float2)*100) / 100
+			fmt.Println(result)
+			i = 1
+		default:
+			fmt.Println("Enter a valid operation!!!")
+		}
 
+	}
 }
